109673b5a91858cbadfdd98fb0b87b9e7f2d021f
sanjeev-iitr-uk <sanjeev@hometown.in>
2020-06-27 09:51:43 +0530

    adding routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cab Booking Service' });
+  res.render('index', { title: 'Fuber' });
 });
+var rideRoutes = require('./ride');
+
+router.use('/ride', rideRoutes)
 module.exports = router;
---
+++ routes/ride.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const rideController = require('../controllers/rideController');
+const fraudDetection = require('../middleware/fraudDetector');
+const requestValidator = require('../middleware/requestValidator');
+const startRideValidator = requestValidator.searchRideValidator;
+const endRideValidator = requestValidator.searchRideValidator;
+const searchRideValidator = requestValidator.searchRideValidator;
+const router = express.Router();
+
+// GET /ride
+router.get('/start', startRideValidator, fraudDetection, rideController.startRide);
+router.get('/end', endRideValidator, fraudDetection, rideController.endRide);
+router.get('/search', searchRideValidator, fraudDetection, rideController.cabsList);
+
+module.exports = router;
