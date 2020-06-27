1d0be07e7ed2b289349060abf3cf4383f0d8453b
sanjeev-iitr-uk <sanjeev@hometown.in>
2020-06-27 09:53:35 +0530

    adding middlewares

---
+++ middleware/fraudDetector.js
@@ -0,0 +1,4 @@
+module.exports = (req, res, next) => {
+  // we can put fraud detector validators here
+  next();
+};
---
+++ middleware/requestValidator.js
@@ -0,0 +1,47 @@
+const startRideValidator = (req, res, next) => {
+  if (!req.query.lattitude || !req.query.longitude || isNaN(req.query.lattitude) || isNaN(req.query.longitude)) {
+    // const error = new Error('Invalid Parameters !');
+    // error.statusCode = 400;
+    // throw error;
+    res.json({
+      message: "Invalid/Missing parameters"
+    });
+  }
+  next();
+};
+const endRideValidator = (req, res, next) => {
+  if (!req.query.lattitude || !req.query.longitude || isNaN(req.query.lattitude) || isNaN(req.query.longitude)) {
+    // const error = new Error('Invalid Parameters !');
+    // error.statusCode = 400;
+    // throw error;
+    res.json({
+      message: "Invalid/Missing parameters"
+    });
+  }
+  next();
+};
+const searchRideValidator = (req, res, next) => {
+  if (
+    !req.query.lattitude ||
+    !req.query.longitude ||
+    isNaN(req.query.lattitude)||
+    isNaN(req.query.longitude ||
+    !req.query.radius ||
+    isNaN(req.query.radius)
+    )) {
+    // const error = new Error('Invalid Parameters !');
+    // error.statusCode = 400;
+    // throw error;
+    res.json({
+      message: "Invalid/Missing parameters"
+    });
+  }
+  next();
+};
+
+module.exports = {
+  startRideValidator,
+  endRideValidator,
+  searchRideValidator
+};
+
