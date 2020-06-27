981e05b86ef5b5e3430028599e7a3aa9f7fb305d
sanjeev-iitr-uk <sanjeev@hometown.in>
2020-06-27 09:59:56 +0530

    adding views

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
